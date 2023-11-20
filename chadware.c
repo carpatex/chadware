540a2bd7b6006abd2c4d10a898f2217613398f70
carpatex <imls.ramirez.tomas@gmail.com>
2023-11-20 13:45:04 -0300

    2nd based commit

--- chadware.c
+++ chadware.c
@@ -1,34 +1,39 @@
 #include "chadware.h"
-static const GLOBALVAR_PROPORTION = 5;
-static const DATAIN_PROPORTION = 10;
-static const DATAOUT_PROPORTION = 10;
-static const CHUNK_PROPORTION = 35;
+static const size_t GLOBALVAR_PROPORTION = 5;
+static const size_t DATAIN_PROPORTION = 10;
+static const size_t DATAOUT_PROPORTION = 10;
+static const size_t CHUNK_PROPORTION = 35;
 /* static const ENTITY_PROPORTION = 40; */ 
 /* este no va porque es al 
 	 pedo ya que al ser el último se lleva el resto de la memoria
 	 por descarte. */
 
 static void *heap;
-static size_t *globalvar_ptr, *datain_ptr, *dataout_ptr, *chunk_ptr, *entity_ptr,*heapsize_ptr; 
+static void **globalvar_ptr; 
+static void **datain_ptr;
+static void **dataout_ptr;
+static void **chunk_ptr;
+static void **entity_ptr; 
+static void **heapsize_ptr; 
 /* punteros a las primeras cinco variables de la heap, que marcan 
 	 donde carajo empieza cada una de esas secciones. el último, 
 	 heapsize_ptr, marca el final de la heap y por consiguiente, 
 	 el tamaño de la misma */
 
 void* init_heap_chadware(const size_t heap_size){
-	if (((GLOBAL_VAR_PROPORTION * heap_size) / 100) < (5 * sizeof(size_t))) {
+	if (((GLOBALVAR_PROPORTION * heap_size) / 100) < (5 * sizeof(size_t))) {
 		return NULL;
 	} else {
 		heap = calloc(1, heap_size);
 
 		heapsize_ptr = heap;
-		globalvar_ptr = heap + (1 * sizeof(size_t));
-		datain_ptr = heap + (2 * sizeof(size_t));
-		dataout_ptr = heap + (3 * sizeof(size_t));
-		chunk_ptr = heap + (4 * sizeof(size_t));
-		entity_ptr = heap + (5 * sizeof(size_t));
+		globalvar_ptr = heap + (1 * sizeof(void *));
+		datain_ptr = heap + (2 * sizeof(void *));
+		dataout_ptr = heap + (3 * sizeof(void *));
+		chunk_ptr = heap + (4 * sizeof(void *));
+		entity_ptr = heap + (5 * sizeof(void *));
 		
-		*heapsize_ptr = heap_size;
+		*heapsize_ptr = (void *) heap_size;
 		*globalvar_ptr = heap;
 		*datain_ptr = heap + ((GLOBALVAR_PROPORTION * heap_size) / 100);
 		*dataout_ptr = *datain_ptr + ((DATAIN_PROPORTION * heap_size) / 100);
