d893583ec0aad6079999f866533aeb3b9379989c
carpatex <imls.ramirez.tomas@gmail.com>
2024-09-15 19:07:48 -0300

    p69

--- packed.c
+++ packed.c
@@ -5,7 +5,6 @@
 void compress_chunk32(struct Chunk *chunk, int* packed_values_used, int max_packed_values, struct PackedValues32* result) {
 	int i, j, start_x, start_y, end_x, end_y, count = 0;
 	int32_t value;
-
 	for (i = 0; i < CHUNK_N_TILES; i++) {
 		for (j = 0; j < CHUNK_N_TILES; j++) {
 			if (i == 0 || j == 0 || (chunk)->tile[i][j] != (chunk)->tile[i-1][j] || (chunk)->tile[i][j] != (chunk)->tile[i][j-1]) {
@@ -25,12 +24,12 @@ void compress_chunk32(struct Chunk *chunk, int* packed_values_used, int max_pack
 				result[count].start_pos_y = start_y;
 				result[count].end_pos_x = end_x - 1;
 				result[count].end_pos_y = end_y - 1;
-       				if (count >= max_packed_values) {
-	        			*packed_values_used = count;
+				if (count >= max_packed_values) {
+					*packed_values_used = count;
 					return;
-        			} else {
+				} else {
 					count++;
-        			}
+				}
 			}
 		}
 	}
@@ -38,9 +37,9 @@ void compress_chunk32(struct Chunk *chunk, int* packed_values_used, int max_pack
 }
 
 void decompress_chunk32(struct PackedValues32* packed_values, int n_packed_values, struct Chunk *chunk) {
-    int i, start_x, start_y, end_x, end_y;
+	int i, start_x, start_y, end_x, end_y;
 	int32_t value;
-    for (i = 0; i < n_packed_values; i++) {
+	for (i = 0; i < n_packed_values; i++) {
 		start_x = packed_values[i].start_pos_x;
 		start_y = packed_values[i].start_pos_y;
 		end_x = packed_values[i].end_pos_x;
@@ -79,11 +78,11 @@ void compress_chunk16(struct Chunk *chunk, int* packed_values_used, int max_pack
 				result[count].end_pos_x = end_x - 1;
 				result[count].end_pos_y = end_y - 1;
 				if (count >= max_packed_values) {
-	 				*packed_values_used = count;
-        				return;
-			        } else {
-					  count++;
-        			}
+					*packed_values_used = count;
+					return;
+				} else {
+					count++;
+				}
 			}
 		}
 	}
@@ -91,9 +90,9 @@ void compress_chunk16(struct Chunk *chunk, int* packed_values_used, int max_pack
 }
 
 void decompress_chunk16(struct PackedValues16* packed_values, int n_packed_values, struct Chunk *chunk) {
-    int x, y, i, start_x, start_y, end_x, end_y;
+	int x, y, i, start_x, start_y, end_x, end_y;
 	int16_t value;
-    for (i = 0; i < n_packed_values; i++) {
+	for (i = 0; i < n_packed_values; i++) {
 		start_x = packed_values[i].start_pos_x;
 		start_y = packed_values[i].start_pos_y;
 		end_x = packed_values[i].end_pos_x;
