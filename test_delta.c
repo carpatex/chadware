d4fac9c41f7be619ac116558a0809e4c53bad726
carpatex <imls.ramirez.tomas@gmail.com>
2024-10-11 18:30:27 -0300

    finalmente lo logre

--- test_delta.c
+++ test_delta.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 #include <time.h>
-#define TPS_OBJECTIVE 20.0
-#define NEXT_TICK CLOCKS_PER_SEC / TPS_OBJECTIVE + .01 
-void operacion() { //algo random que suma hasta una suma determinada 
-	int i, j;
+#define TPS_OBJECTIVE 20
+#define NEXT_TICK CLOCKS_PER_SEC / TPS_OBJECTIVE 
+void tick() { //algo random que suma hasta un numero basado 
+	int j;
 	j = 0;
-	while (j < 5000)
+	while (j < 6969)
 		j++;
 }
 
 int main() {
 	int delta, oldtime;
-	double tps, next_tick;
-	int ops_per_second = 0;
+	double tps;
 	while (1) {
 		delta = clock() - oldtime;
 		tps = (1.0 / delta) * CLOCKS_PER_SEC;
 		oldtime = clock();
-		while (clock() < oldtime + NEXT_TICK) {
-			operacion();
-			ops_per_second++;
-		}
-		printf("delta: %d | op_hechas: %d | tps: %lf\n", delta, ops_per_second, tps);
-		ops_per_second = 0;
+		tick();
+		while(clock() < oldtime + NEXT_TICK);
+		printf("delta: %d | tps: %lf\n", delta, tps);
 	}
 	return 0;
 }
