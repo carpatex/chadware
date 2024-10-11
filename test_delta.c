9dabb3c90500e598960791903fac35af0df8fbc9
carpatex <imls.ramirez.tomas@gmail.com>
2024-10-11 15:52:00 -0300

    delta time update

---
+++ test_delta.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+#define TPS_OBJECTIVE 64.0
+#define NEXT_TICK CLOCKS_PER_SEC / TPS_OBJECTIVE 
+void operacion() { //algo random que suma hasta una suma determinada 
+	int i, j;
+	j = 0;
+	while (j < 5000)
+		j++;
+}
+
+int main() {
+	int delta, oldtime;
+	double tps, next_tick;
+	int ops_per_second = 0;
+	while (1) {
+		delta = clock() - oldtime;
+		tps = (1.0 / delta) * CLOCKS_PER_SEC;
+		oldtime = clock();
+		while (clock() < oldtime + NEXT_TICK) {
+			operacion();
+			ops_per_second++;
+		}
+		printf("delta: %d | op_hechas: %d | tps: %lf\n", delta, ops_per_second, tps);
+		ops_per_second = 0;
+	}
+	return 0;
+}
